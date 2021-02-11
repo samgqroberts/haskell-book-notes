f39bd694ee61340477652cbd5907f7e5e9180e39
Sam Roberts <samgqroberts@gmail.com>
2021-02-11 17:30:06 -0500

    (kinda) the rest of chapter15 exercises

--- chapter15/exercises.hs
+++ chapter15/exercises.hs
@@ -151,25 +151,150 @@ checkOr = quickCheck (semigroupAssoc :: OrAssoc String String)
 -- 9.
 
 -- DONT KNOW THIS ONE
+-- BECAUSE IT IS POPPYCOCK
 -- it says "The type of functions should already have an Arbitrary instance that you can reuse for testing this instance."
 --   but i have no clue how to generate an arbitrary fn
 
-newtype Combine a b = Combine { unCombine :: (a -> b) }
+-- newtype Combine a b = Combine { unCombine :: a -> b }
 
-instance Semigroup b => Semigroup (Combine a b) where
-  (Combine f) <> (Combine g) = Combine (\a -> f a <> g a)
+-- instance Semigroup b => Semigroup (Combine a b) where
+--   (Combine f) <> (Combine g) = Combine (\a -> f a <> g a)
 
-instance (Arbitrary b) => Arbitrary (Combine a b) where
+-- instance (Arbitrary b) => Arbitrary (Combine a b) where
+--   arbitrary = do
+--     -- (f :: a -> b) <- arbitrary
+--     b <- arbitrary
+--     let f = const b
+--     return (Combine f)
+
+-- type CombineAssoc a b = Combine a b -> Combine a b -> Combine a b -> Bool
+
+-- instance Show (Combine a b) where
+--   show (Combine _) = "Combine a b"
+
+-- instance Eq (Combine a b) where
+--   (==) (Combine x) (Combine y) = x == y
+
+-- -- combineAssociation :: Combine a b -> Combine a b -> Combine a b -> Bool
+-- -- combineAssociation c1 c2 c3 = (a $ unCombine (c1 <> (c2 <> c3))) == (a $ unCombine ((c1 <> c2) <> c3))
+
+-- checkCombine :: IO ()
+-- checkCombine = quickCheck (semigroupAssoc :: CombineAssoc String String)
+
+-- f = Combine $ \n -> Sum (n + 1)
+-- g = Combine $ \n -> Sum (n - 1)
+-- h = unCombine (f <> g) $ 0
+
+-- 10. also skipping...
+
+-- 11. 
+
+data Validation a b =
+  Failure' a | Success' b deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+  (<>) (Success' b) (Success' b') = Success' b
+  (<>) (Success' b) (Failure' a) = Success' b
+  (<>) (Failure' a) (Success' b) = Success' b
+  (<>) (Failure' a) (Failure' a') = Failure' (a <> a')
+
+validationMain = do
+  let failure :: String -> Validation String Int
+      failure = Failure'
+      success :: Int -> Validation String Int
+      success = Success'
+  print $ success 1 <> failure "blah"
+  print $ failure "woot" <> failure "blah"
+  print $ success 1 <> success 2
+  print $ failure "woot" <> success 2
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
   arbitrary = do
-    -- (f :: a -> b) <- arbitrary
+    a <- arbitrary
     b <- arbitrary
-    let f = \x -> b
-    return (Combine f)
+    frequency [(1, return (Failure' a)), (1, return (Success' b))]
+  
+type ValidationAssoc a b = Validation a b -> Validation a b -> Validation a b -> Bool
 
-type CombineAssoc a b = Combine a b -> Combine a b -> Combine a b -> Bool
+checkValidation = quickCheck (semigroupAssoc :: ValidationAssoc String Int)
 
-combineAssociation :: Combine a b -> Combine a b -> Combine a b -> Bool
-combineAssociation a c1 c2 c3 = (a $ unCombine (c1 <> (c2 <> c3))) == (a $ unCombine ((c1 <> c2) <> c3))
+{-| Monoid exercises -}
 
-checkCombine :: IO ()
-checkCombine = quickCheck (semigroupAssoc' :: CombineAssoc String String)
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+-- 1.
+
+instance Monoid Trivial where
+  mempty = Trivial
+  mappend = (<>)
+
+checkTrivial' = do
+  let sa = semigroupAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (sa :: TrivAssoc)
+  quickCheck (mli :: Trivial -> Bool)
+  quickCheck (mlr :: Trivial -> Bool)
+
+-- 2.
+
+instance (Monoid a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
+
+checkIdentity' = do
+  let sa = semigroupAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (sa :: IdenAssoc String)
+  quickCheck (mli :: Identity String -> Bool)
+  quickCheck (mlr :: Identity String -> Bool)
+
+-- 3.
+
+instance (Monoid a, Monoid b) => Monoid (Two a b) where
+  mempty = Two mempty mempty
+  mappend = (<>)
+
+checkTwo' = do
+  let sa = semigroupAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (sa :: TwoAssoc String String)
+  quickCheck (mli :: Two String String -> Bool)
+  quickCheck (mlr :: Two String String -> Bool)
+
+-- 4.
+
+instance Monoid BoolConj where
+  mempty = BoolConj True
+
+checkBoolConj' = do
+  let sa = semigroupAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (sa :: BoolConjAssoc)
+  quickCheck (mli :: BoolConj -> Bool)
+  quickCheck (mlr :: BoolConj -> Bool)
+
+-- 5.
+
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+
+checkBoolDisj' = do
+  let sa = semigroupAssoc
+      mli = monoidLeftIdentity
+      mlr = monoidRightIdentity
+  quickCheck (sa :: BoolDisjAssoc)
+  quickCheck (mli :: BoolDisj -> Bool)
+  quickCheck (mlr :: BoolDisj -> Bool)
+
+-- 6. skipping... because it's poppycock
+
+-- 7. also skipping...
+
+-- 8. also skipping...
