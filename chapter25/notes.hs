371fd3a46d320d5ec2d61c7395ae817f9f601592
Sam Roberts <samgqroberts@gmail.com>
2022-07-20 20:49:08 -0400

    chapter 25 complete

--- chapter25/notes.hs
+++ chapter25/notes.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
 
+import Control.Monad (join)
+
 newtype Identity a = Identity { runIdentity :: a } deriving (Show)
 
 newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
@@ -109,4 +111,38 @@ instance Bifunctor Either where
   type-land. So I think that's the leap that makes Monads tougher to visualize. Structure of structure.
   It's not two structures, it's nested structure. So that's where we need to do stuff like
   fmap a <*> into the top structure...
+-}
+
+newtype IdentityT f a =
+  IdentityT { runIdentityT :: f a }
+  deriving (Eq, Show)
+
+instance (Functor m) => Functor (IdentityT m) where
+  fmap f (IdentityT fa) = IdentityT (fmap f fa)
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity a) = Identity (f a)
+
+instance (Applicative m) => Applicative (IdentityT m) where
+  pure x = IdentityT (pure x)
+  (IdentityT fab) <*> (IdentityT fa) = IdentityT (fab <*> fa)
+
+instance Monad Identity where
+  return = pure
+  (Identity a) >>= f = f a
+
+instance (Monad m) => Monad (IdentityT m) where
+  return = pure
+  (>>=) :: IdentityT m a
+        -> (a -> IdentityT m b)
+        -> IdentityT m b
+  (IdentityT ma) >>= f = IdentityT $ ma >>= runIdentityT . f
+
+
+{-|
+  So the "extra type information" we needed and got from nailing down one of the
+  Monad instances in the composition of Monads, here IdentityT, is NOT just the pattern match
+  (IdentityT ma) in the bind definition, like I thought.
+  It's the *fold* that `runIdentityT` affords us, to address the `f (g (f b))` problem.
 -}
